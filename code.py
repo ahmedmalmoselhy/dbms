1be60789574fc73082e262b15b0005ae2543d106
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2018-10-09 08:49:05 +0200

    adding a function to clear the file

--- code.py
+++ code.py
@@ -35,20 +35,41 @@ class Database:
         else :
             print("Removed Successfully ! ")
 
-
     def addRecord(self, *args):
         # Check if data entered = number of columns in the table
         if args.__len__() < colNumber:
             print("Error in Entering Data")
         else:
+            # adding data inside the table
             fh = fh = open(str(self.name+".txt") , "a")
             for i in args:
                 fh.write(i)
                 fh.write("\t\t\t")
+            fh.write("\n")
 
-
+    def clearFile(self):
+        fh = open(str(self.name+".txt"),'w')
+        fh.write("")
 # testing
 students = Database("students")
-students.addTable("names" , "First_Name" , "Second_Name" , "Department" , "Phone_Number")
+# students.addTable("names" , "First_Name" , "Second_Name" , "Department" , "Phone_Number")
 # students.removeTable("names")
-students.addRecord("Ahmed", "Hani", "CSED", "11111")
+# students.addRecord("Ahmed", "Hani", "CSED", "11111")
+# students.addRecord("Ali", "Adel", "ECED", "25556")
+
+print("Please select an option to continue\n")
+print("1) Add a table\n2)Add a record\n3)Clear the whole file")
+while True:
+    opt = input("Inter A Number \t\t:")
+    if opt == '1':
+        students.addTable("names", "First_Name", "Second_Name", "Department", "Phone_Number")
+        print("Table added successfully")
+    elif opt == '2':
+        students.addRecord("Ahmed", "Hani", "CSED", "11111")
+        print("Record added successfully")
+    elif opt == '3':
+        students.clearFile()
+        print("File cleared successfully")
+        students = Database("students")
+    else:
+        print("Wrong Input, Try Again")
